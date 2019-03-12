8b56d30e0e8fd416839d392f3ea4255edbbe46ca
Connie Chan <conniechan.10@gmail.com>
2019-03-12 00:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ function distanceFromHqInFeet(block) {
 }
 
 function distanceTravelledInFeet(start, destination) {
+  let feetTraveled = distanceTravelledInFeet(start, destination)
  return Math.abs((start - destination) * feetPerBlock)
 }
 
 function calculatesFarePrice(start, destination) {
   if (distanceTravelledInFeet(start, destination) < 400) {
     return 0
-  } else if (distanceTravelledInFeet(start, destination ))
+  } else if (distanceTravelledInFeet(start, destination) => 400 && dist)
 }
